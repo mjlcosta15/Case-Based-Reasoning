48f29a01e56f20b391052cea94ce8235c7608381
diogopulp <diogombw@gmail.com>
2017-06-28 15:53:49 +0100

    Concat is not an option

--- Alinea 2/script.m
+++ Alinea 2/script.m
@@ -11,38 +11,34 @@ percentage = round(rows*0.7); % 70 por cento das linhas
 
 vec = randperm(percentage); % gera um vetor de numeros aleatorios compreendidos entre 1 e 649
 
-entry = strcat('school',',','sex',',','age',',','address',',','famsize',',','Pstatus',',','Medu',',','Fedu',',','Mjob',',','Fjob',',','reason',',','guardian',',','traveltime',',','studytime',',','failures',',','schoolsup',',','famsup',',','paid',',','activities',',','nursery',',','higher',',','internet',',','romantic',',','famrel',',','freetime',',','goout',',','Dalc',',','Walc',',','health',',','absences',',','G1',',','G2',',','G3');
+entry = strcat('school',';','sex',';','age',';','address',';','famsize',';','Pstatus',';','Medu',';','Fedu',';','Mjob',';','Fjob',';','reason',';','guardian',';','traveltime',';','studytime',';','failures',';','schoolsup',';','famsup',';','paid',';','activities',';','nursery',';','higher',';','internet',';','romantic',';','famrel',';','freetime',';','goout',';','Dalc',';','Walc',';','health',';','absences',';','G1',';','G2',';','G3');
 
 dlmwrite('por.csv',entry,'Delimiter','','-append');
 
+
+%-------------Grava .csv-----------%
 for i = 1 : length(vec) % percorre o vetor
-    
     
     row1 = int2str(i); % define intervalo
     row2 = int2str(i+1);
     
     table = readtable(filename, 'Range',[row1,':',row2]); % le intervalo
     
-    row = table2cell(table); % linha completa
+    row = table2cell(table); % linha completa 
     
     entry = '';
-    entry = strcat(entry, row,',');
+    
+    for j = 1 : columns
         
-    dlmwrite('por.csv',entry,'Delimiter','','-append');
-
-    
-    %disp(cell);
-    
-    %csvname = strcat(row1,'.csv');
-    
-    %writetable(data,csvname,'Delimiter',',');
-    
-    
-    %dlmwrite('characters.csv', entry, 'delimiter', ';', '-append');
+        entry = row{i,j};
+        %entry = strcat(entry,';');
+        dlmwrite('por.csv',entry,'Delimiter','','-append');
+ 
+    end
     
 end
 
-clc;
+%clc;
 clearvars;
  
 
